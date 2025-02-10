b1223b836c467b74a89f3cac765e88e148ce76b4
Fox Tail <foxtail25@gmail.com>
2025-02-10 13:34:16 +0300

    adaptiv 1200px header + halfmain

--- js/window.js
+++ js/window.js
@@ -3,9 +3,9 @@ const bg_canvas = document.getElementById('bg_canvas_id')
 function getWindowWidth() {
 	const windowInnerWidth = document.documentElement.clientWidth;
 	console.log(windowInnerWidth);
-	+windowInnerWidth < 1160 
+	+windowInnerWidth < 1400 
 	?	bg_canvas.style.width = windowInnerWidth + 'px'
-	:	bg_canvas.style.width = '1160px'
+	:	bg_canvas.style.width = '1400px'
 	 
 }
 getWindowWidth()
