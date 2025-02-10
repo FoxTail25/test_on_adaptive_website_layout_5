9ae3c24a9dd1b9c4b46faff3d4e21d7835eff903
Grigoriy <foxtail25@gmail.com>
2025-02-10 09:24:26 +0300

    Ограничения  main_bg

--- js/window.js
+++ js/window.js
@@ -2,8 +2,11 @@ const bg_canvas = document.getElementById('bg_canvas_id')
 
 function getWindowWidth() {
 	const windowInnerWidth = document.documentElement.clientWidth;
-	console.log(windowInnerWidth)
-	bg_canvas.style.width = windowInnerWidth + 'px';
+	console.log(windowInnerWidth);
+	+windowInnerWidth < 1160 
+	?	bg_canvas.style.width = windowInnerWidth + 'px'
+	:	bg_canvas.style.width = '1160px'
+	 
 }
 getWindowWidth()
 
